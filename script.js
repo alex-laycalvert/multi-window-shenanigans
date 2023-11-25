6f2b64abdbadce801c2af143c62d454e6912eb51
Alexander Lay-Calvert <alex.laycalvert@gmail.com>
2023-11-25 17:55:19 -0500

    turning

--- script.js
+++ script.js
@@ -9,37 +9,42 @@ const Colors = {
     BLACK: {
         color: 0x000000,
         side: THREE.DoubleSide,
-        linewidth: 4,
-        linecap: "round",
-        linejoin: "round",
-    },
-    GRAY: {
-        color: 0x888888,
-        side: THREE.DoubleSide,
     },
     RED: {
         color: 0xff0000,
         side: THREE.DoubleSide,
+        transparent: false,
+        opacity: 1,
     },
     ORANGE: {
         color: 0xff9000,
         side: THREE.DoubleSide,
+        transparent: false,
+        opacity: 1,
     },
     YELLOW: {
         color: 0xffff00,
         side: THREE.DoubleSide,
+        transparent: false,
+        opacity: 1,
     },
     GREEN: {
         color: 0x00ff00,
         side: THREE.DoubleSide,
+        transparent: false,
+        opacity: 1,
     },
     BLUE: {
         color: 0x0000ff,
         side: THREE.DoubleSide,
+        transparent: false,
+        opacity: 1,
     },
     WHITE: {
         color: 0xffffff,
         side: THREE.DoubleSide,
+        transparent: false,
+        opacity: 1,
     },
 };
 
@@ -54,7 +59,7 @@ const Settings = {
 
 async function main() {
     const raycaster = new THREE.Raycaster();
-    const pointer = new THREE.Vector2(-1, -1);
+    const pointer = new THREE.Vector2();
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
         Settings.FIELD_OF_VIEW,
@@ -71,50 +76,62 @@ async function main() {
 
     camera.position.z = 5;
 
-    let clicked = false;
+    const mouse = {
+        moving: false,
+        x: 0,
+        y: 0,
+    };
+
+    /** @type THREE.Mesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.Material | THREE.Material[], THREE.Object3DEventMap> | null */
+    let current = null;
+
+    window.addEventListener("mousedown", (e) => {
+        if (!current) {
+            mouse.moving = true;
+            return;
+        }
+        rotateCube(
+            cube,
+            current,
+            e.button === 0 ? "CLOCKWISE" : "COUNTERCLOCKWISE",
+        );
+    });
+    window.addEventListener("mouseup", () => {
+        mouse.moving = false;
+    });
+
+    window.addEventListener("mousemove", (e) => {
+        if (mouse.moving) {
+            const deltaX = e.clientX - mouse.x;
+            const deltaY = e.clientY - mouse.y;
+            scene.rotateOnWorldAxis(Y, deltaX / 250);
+            scene.rotateOnWorldAxis(X, deltaY / 250);
+        }
+        mouse.x = e.clientX;
+        mouse.y = e.clientY;
+    });
 
     window.addEventListener("wheel", (e) => {
-        if (e.shiftKey) {
-            camera.position.z += e.deltaY / 500;
-            return;
-        }
-        if (e.ctrlKey) {
-            scene.rotateOnWorldAxis(Z, e.deltaY / 1000);
-            return;
-        }
-
-        const faces = [
-            cube[0][2][0],
-            cube[0][2][1],
-            cube[0][2][2],
-            cube[1][2][0],
-            cube[1][2][1],
-            cube[1][2][2],
-            cube[2][2][0],
-            cube[2][2][1],
-            cube[2][2][2],
-        ];
-
-        if (!clicked) {
-            scene.rotateOnWorldAxis(X, e.deltaY / 1000);
-            scene.rotateOnWorldAxis(Y, e.deltaX / 1000);
-            return;
-        }
-
-        faces.flat(2).forEach((mesh) => {
-            mesh.rotateOnWorldAxis(Y, e.deltaY / 1000);
-        });
+        camera.position.z += e.deltaY / 500;
     });
 
     window.addEventListener("pointermove", (e) => {
         pointer.x = (e.clientX / Settings.WIDTH) * 2 - 1;
-        pointer.y = (e.clientY / Settings.HEIGHT) * 2 - 1;
+        pointer.y = (-e.clientY / Settings.HEIGHT) * 2 + 1;
     });
 
+    window.addEventListener("contextmenu", (e) => e.preventDefault());
+
+    colorCube(cube);
     function render() {
         window.requestAnimationFrame(render);
         raycaster.setFromCamera(pointer, camera);
-        colorCube(cube);
+        const intersections = raycaster
+            .intersectObjects(scene.children, false)
+            .filter((i) => i.object instanceof THREE.Mesh);
+        /** @type THREE.Mesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.Material | THREE.Material[], THREE.Object3DEventMap> */
+        const object = intersections[0]?.object;
+        current = object ?? null;
         renderer.render(scene, camera);
     }
 
@@ -137,6 +154,266 @@ function absSum(...nums) {
 }
 
 /**
+ * @param {THREE.Mesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.Material | THREE.Material[], THREE.Object3DEventMap>[][][][]} cube
+ * @param {THREE.Mesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.Material | THREE.Material[], THREE.Object3DEventMap>} face
+ * @param {'CLOCKWISE'|'COUNTERCLOCKWISE'} rotation
+ */
+function rotateCube(cube, face, rotation) {
+    const rotationFactor = rotation === "CLOCKWISE" ? -1 : 1;
+    if (face === cube[2][1][1][0]) {
+        // RED
+        const meshes = [
+            ...cube[2][0][0],
+            ...cube[2][0][1],
+            ...cube[2][0][2],
+            ...cube[2][1][0],
+            ...cube[2][1][1],
+            ...cube[2][1][2],
+            ...cube[2][2][0],
+            ...cube[2][2][1],
+            ...cube[2][2][2],
+        ];
+        meshes.forEach((mesh) => {
+            mesh.rotateOnWorldAxis(Z, (rotationFactor * Math.PI) / 2);
+        });
+        if (rotation === "COUNTERCLOCKWISE") {
+            // adjust corners
+            let tmp = cube[2][2][0];
+            cube[2][2][0] = cube[2][2][2];
+            cube[2][2][2] = cube[2][0][2];
+            cube[2][0][2] = cube[2][0][0];
+            cube[2][0][0] = tmp;
+            // adjust edges
+            tmp = cube[2][1][0];
+            cube[2][1][0] = cube[2][2][1];
+            cube[2][2][1] = cube[2][1][2];
+            cube[2][1][2] = cube[2][0][1];
+            cube[2][0][1] = tmp;
+        } else {
+            // adjust corners
+            let tmp = cube[2][2][0];
+            cube[2][2][0] = cube[2][0][0];
+            cube[2][0][0] = cube[2][0][2];
+            cube[2][0][2] = cube[2][2][2];
+            cube[2][2][2] = tmp;
+            // adjust edges
+            tmp = cube[2][1][0];
+            cube[2][1][0] = cube[2][0][1];
+            cube[2][0][1] = cube[2][1][2];
+            cube[2][1][2] = cube[2][2][1];
+            cube[2][2][1] = tmp;
+        }
+    } else if (face === cube[0][1][1][0]) {
+        // ORANGE
+        const meshes = [
+            ...cube[0][0][0],
+            ...cube[0][0][1],
+            ...cube[0][0][2],
+            ...cube[0][1][0],
+            ...cube[0][1][1],
+            ...cube[0][1][2],
+            ...cube[0][2][0],
+            ...cube[0][2][1],
+            ...cube[0][2][2],
+        ];
+        meshes.forEach((mesh) => {
+            mesh.rotateOnWorldAxis(Z, (rotationFactor * Math.PI) / 2);
+        });
+        if (rotationFactor === 1) {
+            // adjust corners
+            let tmp = cube[0][2][0];
+            cube[0][2][0] = cube[0][2][2];
+            cube[0][2][2] = cube[0][0][2];
+            cube[0][0][2] = cube[0][0][0];
+            cube[0][0][0] = tmp;
+            // adjust edges
+            tmp = cube[0][1][0];
+            cube[0][1][0] = cube[0][2][1];
+            cube[0][2][1] = cube[0][1][2];
+            cube[0][1][2] = cube[0][0][1];
+            cube[0][0][1] = tmp;
+        } else if (rotationFactor === -1) {
+            // adjust corners
+            let tmp = cube[0][2][0];
+            cube[0][2][0] = cube[0][0][0];
+            cube[0][0][0] = cube[0][0][2];
+            cube[0][0][2] = cube[0][2][2];
+            cube[0][2][2] = tmp;
+            // adjust edges
+            tmp = cube[0][1][0];
+            cube[0][1][0] = cube[0][0][1];
+            cube[0][0][1] = cube[0][1][2];
+            cube[0][1][2] = cube[0][2][1];
+            cube[0][2][1] = tmp;
+        }
+    } else if (face === cube[1][1][2][0]) {
+        // BLUE
+        const meshes = [
+            ...cube[0][0][2],
+            ...cube[1][0][2],
+            ...cube[2][0][2],
+            ...cube[0][1][2],
+            ...cube[1][1][2],
+            ...cube[2][1][2],
+            ...cube[0][2][2],
+            ...cube[1][2][2],
+            ...cube[2][2][2],
+        ];
+        meshes.forEach((mesh) => {
+            mesh.rotateOnWorldAxis(X, (rotationFactor * Math.PI) / 2);
+        });
+        if (rotation === "COUNTERCLOCKWISE") {
+            // adjust corners
+            let tmp = cube[2][2][2];
+            cube[2][2][2] = cube[0][2][2];
+            cube[0][2][2] = cube[0][0][2];
+            cube[0][0][2] = cube[2][0][2];
+            cube[2][0][2] = tmp;
+            // adjust edges
+            tmp = cube[2][1][2];
+            cube[2][1][2] = cube[1][2][2];
+            cube[1][2][2] = cube[0][1][2];
+            cube[0][1][2] = cube[1][0][2];
+            cube[1][0][2] = tmp;
+        } else {
+            // adjust corners
+            let tmp = cube[2][2][2];
+            cube[2][2][2] = cube[2][0][2];
+            cube[2][0][2] = cube[0][0][2];
+            cube[0][0][2] = cube[0][2][2];
+            cube[0][2][2] = tmp;
+            // adjust edges
+            tmp = cube[2][1][2];
+            cube[2][1][2] = cube[1][0][2];
+            cube[1][0][2] = cube[0][1][2];
+            cube[0][1][2] = cube[1][2][2];
+            cube[1][2][2] = tmp;
+        }
+    } else if (face === cube[1][1][0][0]) {
+        // GREEN
+        const meshes = [
+            ...cube[0][0][0],
+            ...cube[1][0][0],
+            ...cube[2][0][0],
+            ...cube[0][1][0],
+            ...cube[1][1][0],
+            ...cube[2][1][0],
+            ...cube[0][2][0],
+            ...cube[1][2][0],
+            ...cube[2][2][0],
+        ];
+        meshes.forEach((mesh) => {
+            mesh.rotateOnWorldAxis(X, (rotationFactor * Math.PI) / 2);
+        });
+        if (rotation === "COUNTERCLOCKWISE") {
+            // adjust corners
+            let tmp = cube[2][2][0];
+            cube[2][2][0] = cube[0][2][0];
+            cube[0][2][0] = cube[0][0][0];
+            cube[0][0][0] = cube[2][0][0];
+            cube[2][0][0] = tmp;
+            // adjust edges
+            tmp = cube[2][1][0];
+            cube[2][1][0] = cube[1][2][0];
+            cube[1][2][0] = cube[0][1][0];
+            cube[0][1][0] = cube[1][0][0];
+            cube[1][0][0] = tmp;
+        } else {
+            // adjust corners
+            let tmp = cube[2][2][0];
+            cube[2][2][0] = cube[2][0][0];
+            cube[2][0][0] = cube[0][0][0];
+            cube[0][0][0] = cube[0][2][0];
+            cube[0][2][0] = tmp;
+            // adjust edges
+            tmp = cube[2][1][0];
+            cube[2][1][0] = cube[1][0][0];
+            cube[1][0][0] = cube[0][1][0];
+            cube[0][1][0] = cube[1][2][0];
+            cube[1][2][0] = tmp;
+        }
+    } else if (face === cube[1][2][1][0]) {
+        // WHITE
+        const meshes = [
+            ...cube[0][2][0],
+            ...cube[0][2][1],
+            ...cube[0][2][2],
+            ...cube[1][2][0],
+            ...cube[1][2][1],
+            ...cube[1][2][2],
+            ...cube[2][2][0],
+            ...cube[2][2][1],
+            ...cube[2][2][2],
+        ];
+        meshes.forEach((mesh) => {
+            mesh.rotateOnWorldAxis(Y, (rotationFactor * Math.PI) / 2);
+        });
+        if (rotation === "COUNTERCLOCKWISE") {
+            let tmp = cube[0][2][0];
+            cube[0][2][0] = cube[0][2][2];
+            cube[0][2][2] = cube[2][2][2];
+            cube[2][2][2] = cube[2][2][0];
+            cube[2][2][0] = tmp;
+            tmp = cube[1][2][0];
+            cube[1][2][0] = cube[0][2][1];
+            cube[0][2][1] = cube[1][2][2];
+            cube[1][2][2] = cube[2][2][1];
+            cube[2][2][1] = tmp;
+        } else {
+            let tmp = cube[0][2][0];
+            cube[0][2][0] = cube[2][2][0];
+            cube[2][2][0] = cube[2][2][2];
+            cube[2][2][2] = cube[0][2][2];
+            cube[0][2][2] = tmp;
+            tmp = cube[1][2][0];
+            cube[1][2][0] = cube[2][2][1];
+            cube[2][2][1] = cube[1][2][2];
+            cube[1][2][2] = cube[0][2][1];
+            cube[0][2][1] = tmp;
+        }
+    } else if (face === cube[1][0][1][0]) {
+        // YELLOW
+        const meshes = [
+            ...cube[0][0][0],
+            ...cube[0][0][1],
+            ...cube[0][0][2],
+            ...cube[1][0][0],
+            ...cube[1][0][1],
+            ...cube[1][0][2],
+            ...cube[2][0][0],
+            ...cube[2][0][1],
+            ...cube[2][0][2],
+        ];
+        meshes.forEach((mesh) => {
+            mesh.rotateOnWorldAxis(Y, (rotationFactor * Math.PI) / 2);
+        });
+        if (rotation === "COUNTERCLOCKWISE") {
+            let tmp = cube[0][0][0];
+            cube[0][0][0] = cube[0][0][2];
+            cube[0][0][2] = cube[2][0][2];
+            cube[2][0][2] = cube[2][0][0];
+            cube[2][0][0] = tmp;
+            tmp = cube[1][0][0];
+            cube[1][0][0] = cube[0][0][1];
+            cube[0][0][1] = cube[1][0][2];
+            cube[1][0][2] = cube[2][0][1];
+            cube[2][0][1] = tmp;
+        } else {
+            let tmp = cube[0][0][0];
+            cube[0][0][0] = cube[2][0][0];
+            cube[2][0][0] = cube[2][0][2];
+            cube[2][0][2] = cube[0][0][2];
+            cube[0][0][2] = tmp;
+            tmp = cube[1][0][0];
+            cube[1][0][0] = cube[2][0][1];
+            cube[2][0][1] = cube[1][0][2];
+            cube[1][0][2] = cube[0][0][1];
+            cube[0][0][1] = tmp;
+        }
+    }
+}
+
+/**
  * @returns {THREE.Mesh[][][][]} cube
  */
 function generateCube() {
@@ -154,19 +431,19 @@ function generateCube() {
                     geo1.translate((x * 2) / 3, (y * 2) / 3, z);
                     const mesh1 = new THREE.Mesh(
                         geo1,
-                        new THREE.MeshBasicMaterial(Colors.GRAY),
+                        new THREE.MeshBasicMaterial(Colors.BLACK),
                     );
                     const geo2 = new THREE.BoxGeometry(0, s, s);
                     geo2.translate(x, (y * 2) / 3, (z * 2) / 3);
                     const mesh2 = new THREE.Mesh(
                         geo2,
-                        new THREE.MeshBasicMaterial(Colors.GRAY),
+                        new THREE.MeshBasicMaterial(Colors.BLACK),
                     );
                     const geo3 = new THREE.BoxGeometry(s, 0, s);
                     geo3.translate((x * 2) / 3, y, (z * 2) / 3);
                     const mesh3 = new THREE.Mesh(
                         geo3,
-                        new THREE.MeshBasicMaterial(Colors.GRAY),
+                        new THREE.MeshBasicMaterial(Colors.BLACK),
                     );
                     row.push([mesh1, mesh2, mesh3]);
                 } else if (absSum(x, y, z) === l * 2) {
@@ -176,7 +453,7 @@ function generateCube() {
                         geo1.translate((x * 2) / 3, (y * 2) / 3, z);
                         const mesh1 = new THREE.Mesh(
                             geo1,
-                            new THREE.MeshBasicMaterial(Colors.GRAY),
+                            new THREE.MeshBasicMaterial(Colors.BLACK),
                         );
                         const geo2 = new THREE.BoxGeometry(
                             Math.abs(x) === l ? 0 : s,
@@ -186,7 +463,7 @@ function generateCube() {
                         geo2.translate(x, y, (z * 2) / 3);
                         const mesh2 = new THREE.Mesh(
                             geo2,
-                            new THREE.MeshBasicMaterial(Colors.GRAY),
+                            new THREE.MeshBasicMaterial(Colors.BLACK),
                         );
                         row.push([mesh1, mesh2]);
                     } else if (z === 0) {
@@ -194,13 +471,13 @@ function generateCube() {
                         geo1.translate(x, (y * 2) / 3, z);
                         const mesh1 = new THREE.Mesh(
                             geo1,
-                            new THREE.MeshBasicMaterial(Colors.GRAY),
+                            new THREE.MeshBasicMaterial(Colors.BLACK),
                         );
                         const geo2 = new THREE.BoxGeometry(s, 0, s);
                         geo2.translate((x * 2) / 3, y, (z * 2) / 3);
                         const mesh2 = new THREE.Mesh(
                             geo2,
-                            new THREE.MeshBasicMaterial(Colors.GRAY),
+                            new THREE.MeshBasicMaterial(Colors.BLACK),
                         );
                         row.push([mesh1, mesh2]);
                     }
@@ -214,7 +491,7 @@ function generateCube() {
                     geo.translate(x, y, z);
                     const mesh = new THREE.Mesh(
                         geo,
-                        new THREE.MeshBasicMaterial(Colors.GRAY),
+                        new THREE.MeshBasicMaterial(Colors.BLACK),
                     );
                     row.push([mesh]);
                 } else {
@@ -224,6 +501,14 @@ function generateCube() {
             slice.push(row);
         }
         cube.push(slice);
+    }
+    for (const mesh of cube.flat(3)) {
+        mesh.add(
+            new THREE.LineSegments(
+                mesh.geometry,
+                new THREE.LineBasicMaterial(Colors.BLACK),
+            ),
+        );
     }
     return cube;
 }
